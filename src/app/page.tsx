52c51dde2680ecc98ac116ded9a1b7ca5391cd00
HatulaPro <shalev280804@gmail.com>
2023-06-03 16:12:50 +0300

    time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ const MILLIS_IN_DAY = 1000 * 60 * 60 * 24;
 export default function Home() {
 	const startTime = new Date('2023-05-02T21:00:00.000Z');
 	const currentTime = new Date();
-	const endTime = new Date('2023-09-27T21:00:00.000Z');
+	const endTime = new Date('2023-09-28T21:00:00.000Z');
 
 	const donePercent =
 		((currentTime.getTime() - startTime.getTime()) /
